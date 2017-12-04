488b95c7466995f669fb3d6a850fa96b1527bb3d
Mansi Agarwal <mansi.agarwal1410@gmail.com>
2017-12-04 11:46:24 -0800

    Completed pseudoclassical

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,13 @@
-var ForagerBee = function() {
+var ForagerBee = function(color,food) {
+	Bee.call(this,color,food);
+	this.age = 10;
+	this.job = 'find pollen';
+	this.canFly = true;
+	this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.forage = function() {
+	this.treasureChest.push('treasure');
+}
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function(color,food) {
+	Bee.call(this,color,food);
+	this.age = 10;
+	this.job = 'make honey';
+	this.honeyPot = 0;
 };
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+HoneyMakerBee.prototype.makeHoney = function() {
+	this.honeyPot += 1;
+}
+HoneyMakerBee.prototype.giveHoney = function() {
+	this.honeyPot -= 1;
+}
+
 
